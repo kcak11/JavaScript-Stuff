aca38a0ea88a2554f1a63631ea83388adc847888
K.C.Ashish Kumar <kcak11@gmail.com>
2017-03-01 16:43:33 +0530

    Update JSLinkedList.js

--- Data-Structures/JSLinkedList.js
+++ Data-Structures/JSLinkedList.js
@@ -4,46 +4,58 @@ Dt: 01/03/2017
 (c) Ashish's Web - http://www.ashishkumarkc.com
 License: https://kcak11.mit-license.org/
 */
-var llist;
 
-var addToLinkedList=function(elem,nextElem){
-	if(!llist){
-		llist=elem;
-	}
-	if(!elem.getCurrentElem){
-		elem.getCurrentElem=function(){return elem;};
-	}
-	elem.getNextElem=function(){
-		nextElem.getCurrentElem=function(){return nextElem;};
-		return nextElem;
-	};
-};
-
-var traverseList=function(aList,callback){
-	if(!aList || !callback){
-		return;
-	}
-	var cElem=aList.getCurrentElem();
-	var traverse=true;
-	while(cElem && traverse){
-		callback(cElem.getCurrentElem());
-		cElem=cElem.getNextElem && cElem.getNextElem();
-		if(cElem && cElem.getNextElem && !cElem.getNextElem()){
-			traverse=false;
+var LinkedList=function(){
+	this.list;
+	this.currentElem=null;
+	
+	this.addToLinkedList=function(elem){
+		var wrapper={};
+		wrapper.element=elem;
+		if(!this.list){
+			this.list={};
+			this.firstElem=this.list;
+			this.list._nextElem=wrapper;
+		}else{
+			this.currentElem._nextElem=wrapper;
 		}
-	}
+		this.currentElem=wrapper;
+	};
+	
+	this.traverseList=function(callback){
+		if(!callback){
+			return;
+		}
+		var cElem=this.firstElem._nextElem;
+		var traverse=true;
+		callback(cElem.element);
+		while(cElem && cElem._nextElem && traverse){
+			callback(cElem._nextElem.element);
+			if(cElem===cElem._nextElem){
+				traverse=false;
+			}
+			cElem=cElem._nextElem;
+		}
+	};
+		
 };
+
+/*Usage: Begins here*/
+
 var elem1={"a":"1"};
 var elem2={"b":"2"};
 var elem3={"c":"3"};
 var elem4={"d":"4"};
 var elem5={"e":"5"};
 
-addToLinkedList(elem1,elem2);
-addToLinkedList(elem2,elem3);
-addToLinkedList(elem3,elem4);
-addToLinkedList(elem4,elem5);
+var llist=new LinkedList();
 
-traverseList(llist,function(e){
+llist.addToLinkedList(elem1);
+llist.addToLinkedList(elem2);
+llist.addToLinkedList(elem3);
+llist.addToLinkedList(elem4);
+llist.addToLinkedList(elem5);
+
+llist.traverseList(function(e){
 	console.log(e);
 });
