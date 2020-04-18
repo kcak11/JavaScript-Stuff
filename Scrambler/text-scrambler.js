56a2ffad273f8e0e207247c870f41741bf01c3fc
K.C.Ashish Kumar <kcak11@gmail.com>
2020-04-18 14:15:30 +0530

    Add files via upload

--- Scrambler/text-scrambler.js
+++ Scrambler/text-scrambler.js
@@ -4,10 +4,23 @@
  * Author: kcak11 (https://kcak11.com)
  * License: MIT (https://mit-license.kcak11.com)
  */
-((function (_g) {
-    _g.scramble = function (msg, key) {
-        if (!msg || !key) {
-            throw new this.Error("msg & key are mandatory parameters");
+
+/**
+ * @globalObject [MANDATORY]: The globalObject represents the following:
+ * 'window' object when running in the scope of a Browser.
+ * 'global' object when running in the scope of NodeJS.
+ * If both the window & global objects are not available, then the globalObject will be defaulted to an empty object {}
+ */
+((function (globalObject) {
+    /**
+     * @msg [MANDATORY]: The message string that needs to be scrambled/unscrambled
+     * @key [MANDATORY]: The key to be used for scrambling/unscrambling the message
+     * 
+     * @returns: The scrambled/unscrambled message text.
+     */
+    globalObject.scramble = function (msg, key) {
+        if (!msg || !key || typeof msg !== "string" || typeof key !== "string") {
+            throw new this.Error("msg & key are mandatory parameters of type <string>.");
         }
         var SECRETS = [
             "ZJmFM7QHeT3AWc94C2tGf0dqoESKgwlV",
@@ -23,7 +36,7 @@
             "7aOqpklKnUFADi1jNgPzouvJGtbs0YW6"
         ];
         var _key = "";
-        key += SECRETS[key.length % 11];
+        key += SECRETS[(key.length + msg.length) % 11];
         for (var i = 0; i < key.length; i++) {
             var _k = key.charCodeAt(i);
             _key += _k + i + (_k % 11);
@@ -33,9 +46,9 @@
         }
         return Function("msg", decodeURIComponent("var%20_msg%3D%22%22%3B%0Afor(var%20i%3D0%3Bi%3Cmsg.length%3Bi%2B%2B)%7B%0A%20%20%20%20_msg%2B%3DString.fromCharCode(msg.charCodeAt(i)%5E(i%2B1)%5E%7B_key%7D)%3B%0A%7D%0Areturn%20_msg%3B%0A").split("{_key}").join(_key))(msg);
     };
-})((typeof window !== "undefined" ? window : global)));
+})((typeof window !== "undefined" ? window : (typeof global !== "undefined" ? global : {}))));
 
-/**
+/*
  * USAGE:
  * var originalMessage = "my message text";
  * var result = scramble(originalMessage, "my secret key");
